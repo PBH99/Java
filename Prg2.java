3f5b225a3f09cfd26143e632e1cbc7935d494834
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2022-12-09 21:12:45 +0530

    Prg2
    Prg2 - Learn how to print any message in java

---
+++ Prg2.java
@@ -0,0 +1,10 @@
+package pr1;
+
+import java.util.Scanner;
+public class Prg2 {
+
+	 public static void main(String[]args)
+	 {
+		 System.out.print("Now i am going to start my java journy from here  after a long  process i learnt how to use eclipse ide");
+	 }
+}
