23336c202e4a628cec28381cca099a5f326322f8
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2022-12-13 20:09:54 +0530

    Prg5 - Print Both even and odd

---
+++ Prg5.java
@@ -0,0 +1,24 @@
+package prashant;
+import java.util.Scanner;
+
+public class Prg5 {
+
+	public static void main(String[] args) {
+		
+		int n=0,r,sum;
+		System.out.println("Even \t\t Odd");
+		while(n<20)
+		{
+			if(n%2==0)
+			{
+		    System.out.print(n);
+			}
+			else
+			{
+				System.out.println("\t\t"+n);
+			}
+			n++;
+		}
+	}
+
+}
