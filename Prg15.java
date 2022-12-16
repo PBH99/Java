daa623e44fd579cd8a3db250089b231e7e21f3c4
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2022-12-17 01:37:29 +0530

    Prg15 - Print Alphabet (A to Z)

---
+++ Prg15.java
@@ -0,0 +1,16 @@
+package prashant;
+
+public class Prg15 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		while ( ch<='Z')
+		{
+			
+			System.out.print("\t"+ch);
+            ch++;		
+		}
+
+	}
+
+}
