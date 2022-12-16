c810cca46cb95bb52dd321f92f4ce694b4ae0ccb
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2022-12-17 01:39:54 +0530

    Prg16 - String ( Print any messege )
    How to use string and how to input string by user 
    and print string that messege.

---
+++ Prg16.java
@@ -0,0 +1,21 @@
+//            STRING
+
+package prashant;
+import java.util.Scanner;
+public class Prg16 {
+
+	public static void main(String[] args) {
+		
+		String a = "Prashant" ;
+        System.out.println(a);
+       // String b;
+        Scanner s=new Scanner(System.in);
+        //String c = s.next();
+        String b = s.nextLine();
+        System.out.println(b);
+        String c = s.nextLine();
+        System.out.println(c);
+
+	}
+
+}
