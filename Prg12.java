4456c89d3006875a785ad5481d1c1f5483b5e3ac
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2022-12-17 01:34:25 +0530

    Prg12 - Solve the expression b*2 - 4*a*c
    ( By using precedence and associativity )

---
+++ Prg12.java
@@ -0,0 +1,15 @@
+package prashant;
+
+public class Prg12 {
+
+	public static void main(String[] args) {
+	
+		int a=3,b=4,c=6;
+		int l;
+		l=b*b - (4*a*c)/(2*a);
+		System.out.print(l);
+		
+
+	}
+
+}
