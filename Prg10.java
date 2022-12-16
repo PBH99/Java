31f7fb1c4549f3be58f05657e035f3f57b025a83
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2022-12-17 00:16:01 +0530

    Prg10 - Print Hello By using ( Do While )

---
+++ Prg10.java
@@ -0,0 +1,15 @@
+package prashant;
+
+public class Prg10 {
+
+	public static void main(String[] args) {
+		int a=0;
+		do
+		{
+			System.out.print("Hello\n");
+			a++;
+		}
+		while (a<10);
+	}
+
+}
