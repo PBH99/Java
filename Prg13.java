46ea8554806296d5de61522324545e4c986afcea
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2022-12-17 01:35:26 +0530

    Prg13 - Increment and Decrement

---
+++ Prg13.java
@@ -0,0 +1,21 @@
+// INCREMENT AND DECREEMENT
+
+package prashant;
+
+public class Prg13 {
+
+	public static void main(String[] args) {
+		
+     int a=10;
+  // int c = i++; First c is assigned a (10) then a is incremented 
+     int b=12;
+  // int d=++j ; First b incrementd then d is assigned b =(13)
+	
+	System.out.println(a++);
+	System.out.println(a);
+	System.out.println(++b);
+	System.out.println(b);
+	
+	}
+
+}
