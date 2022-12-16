4e3df892dc5bbe693ab89ed7d6e7092e152bb545
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2022-12-17 01:36:31 +0530

    Prg14 - Problem of increment and decrement
    (Find the value of x )

---
+++ Prg14.java
@@ -0,0 +1,15 @@
+/* Q. What will be the value of the following expression (x)
+    int y = 7
+    int x = ++y*8 */
+package prashant;
+
+public class Prg14 {
+
+	public static void main(String[] args) {
+		int y = 7;
+		int x = ++y*8;
+		System.out.print(x);
+
+	}
+
+}
