b8043cdfdba5e2543b81e72cf918fea4fe33a168
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-02-26 14:53:07 +0530

    Prg29 - Find the value of given expression
    Find the value of the following expression :

---
+++ Prg29.java
@@ -0,0 +1,15 @@
+// Find the value of the following expression :
+
+package program;
+
+public class Prg29 {
+
+	public static void main(String[] args) {
+		//int x=7;
+		int a=7*49/7+35/7;
+		System.out.print(a);
+		
+
+	}
+
+}
