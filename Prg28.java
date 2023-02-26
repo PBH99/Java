601f9fdfe93ccf5f07628e14ffb99936d9f257cf
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-02-26 14:52:03 +0530

    Prg28 - Print the expression
     Write the following expression in a given program ::
     v2-u2/2as

---
+++ Prg28.java
@@ -0,0 +1,17 @@
+// Write the following expression in a given program ::
+// v2-u2/2as
+
+package program;
+
+public class Prg28 {
+
+	public static void main(String[] args) {
+		int v=2,u=0,a=5,s=6;
+		float t;
+		t= ((v*v)-(u*u))/2*a*s;
+		System.out.println(t);
+		
+
+	}
+
+}
