2e57c543b8c32f7030e3eb45cdbb04e92bffc200
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-02-26 14:54:16 +0530

    Prg30 - Convert a string to lowercase
    Write a java program to convert a string to lowercase

---
+++ Prg30.java
@@ -0,0 +1,15 @@
+// Write a java program to convert a string to lowercase
+
+package program;
+
+public class Prg30 {
+
+	public static void main(String[] args) {
+		String name = "Prashant bharadwaj";
+		name=name.toLowerCase();
+		System.out.println(name);
+		
+
+	}
+
+}
