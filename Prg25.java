014bc1182388d468465cd06fd8004e8a4042bff2
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-02-26 14:48:06 +0530

    Prg25 - Solve given mathmatical operation
     What will be the result of the following expression 
      float a=7/4*9/2

---
+++ Prg25.java
@@ -0,0 +1,14 @@
+// What will be the result of the following expression 
+//  float a=7/4*9/2
+
+package program;
+
+public class Prg25 {
+
+	public static void main(String[] args) {
+		float a=7/4*9/2;
+		System.out.print(a);
+
+	}
+
+}
