8ed472b0f3167978a4075cd84294b685be6060f3
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-02-26 14:51:06 +0530

    Prg27 - Given number is greates or not
    Use comparision operator to find out whether a given number is greatest than the user entered number or not .

---
+++ Prg27.java
@@ -0,0 +1,18 @@
+// Use comparision operator to find out whether a given number is greatest than the user entered number or not .
+
+package program;
+import java.util.Scanner;
+public class Prg27 {
+
+	public static void main(String[] args) {
+		int y;
+		System.out.println("Enter the value of y :: ");
+		Scanner o=new Scanner(System.in);
+		y=o.nextInt();
+		
+			System.out.println(y>20);
+		
+
+	}
+
+}
