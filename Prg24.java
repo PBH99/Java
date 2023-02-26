2ffc3160fbe09a3bb605b66c92b2ca093bf61c2c
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-02-26 14:46:47 +0530

    Prg24 - Detect intger or not
     Write a program to detect the given number is an integer or not

---
+++ Prg24.java
@@ -0,0 +1,16 @@
+// Write a program to detect the given number is an integer or not
+
+package program;
+import java.util.Scanner;
+public class Prg24 {
+
+	public static void main(String[] args) {
+     int x;
+     System.out.println("Enter the number :: ");
+     Scanner o=new Scanner(System.in);
+   //  x=o.nextInt();
+     System.out.println(o.hasNextInt());
+
+	}
+
+}
