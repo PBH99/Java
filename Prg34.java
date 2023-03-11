0bf5d44bcffb414732b220cfcf895cdf822dc09d
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-03-11 13:17:21 +0530

    Prg34 - Print a string in a squence
    Write a program to formate the following letter using escape sequence character ::
     letter = "Dear prashant , this java course is nice"

---
+++ Prg34.java
@@ -0,0 +1,14 @@
+// Write a program to formate the following letter using escape sequence character ::
+//  letter = "Dear prashant , this java course is nice"
+
+package program;
+
+public class Prg34 {
+
+	public static void main(String[] args) {
+    
+		String letter="Dear Prashant ,\n\t this java course is nice\n\t  Thanks!! ";
+		System.out.print(letter);
+	}
+
+}
