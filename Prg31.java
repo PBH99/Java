11312965fecaef074dbf3d471f55a3b63143e274
Prashant Bharadwaj <119725040+PBH99@users.noreply.github.com>
2023-03-11 13:12:25 +0530

    Prg31 - Replace spaces with underscore
    Write a program to replace spaces to underscore in a given string

---
+++ Prg31.java
@@ -0,0 +1,15 @@
+// Write a java program to replace spaces with underscores
+
+package program;
+
+public class Prg31 {
+
+	public static void main(String[] args) {
+		
+				String texts= "What a beautiful sight is there ";
+				texts = texts.replace(" ","_");
+				System.out.print(texts);
+
+	}
+
+}
